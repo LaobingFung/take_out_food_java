afaae1cb09cfd0ef989252639e0170956dad28df
LaoBing_Fung <FLB_B401@163.com>
2019-02-12 23:33:52 +0800

    add dish class

---
+++ src/Dish.java
@@ -0,0 +1,25 @@
+import java.lang.String;
+
+public class Dish {
+    private Item item;
+    private int count;
+    public Dish(String input) {
+        String[] inputs = input.split(" x ");
+        this.item = Item.getItem(inputs[0]);
+        this.count = Integer.valueOf(inputs[1]);
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public static void test() {
+        Dish dish0 = new Dish("ITEM0001 x 2");
+        System.out.println(dish0.getCount());
+        System.out.println(dish0.getItem().getPrice());
+    }
+}
