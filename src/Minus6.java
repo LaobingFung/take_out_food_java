f802c83186fa8ebcb7ec9d6d27303d9f40fc4c3d
LaoBing_Fung <FLB_B401@163.com>
2019-02-13 01:53:11 +0800

    add a class of promotion by minishing 6

---
+++ src/Minus6.java
@@ -0,0 +1,18 @@
+public class Minus6 extends Promotion{
+    public Minus6() {
+        super("满30减6元");
+    }
+
+    @Override
+    public double computeDiscount(Order order){
+        return order.getTotal() >= 30 ? 6.0 : 0.0;
+    }
+
+    public static void test() {
+        String[] input = {"ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"};
+        Order order = new Order(input);
+        Minus6 promotion0 = new Minus6();
+        System.out.println(promotion0.getType());
+        System.out.println(promotion0.computeDiscount(order));
+    }
+}
