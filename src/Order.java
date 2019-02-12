db8db805fb33e4bf2a80359e2a736ea6c37573bb
LaoBing_Fung <FLB_B401@163.com>
2019-02-13 00:39:33 +0800

    add order class

---
+++ src/Order.java
@@ -0,0 +1,34 @@
+import java.lang.String;
+import java.util.Vector;
+
+public class Order {
+    private Vector<Dish> order;
+    public Order(String[] orderInput) {
+        this.order = new Vector<Dish>();
+        for(String str : orderInput) {
+            this.order.add(new Dish(str));
+        }
+    }
+
+    public Vector<Dish> getOrder() {
+        return order;
+    }
+
+    public double getTotal() {
+        double total = 0.0;
+        for(Dish dish : this.order) {
+            total += dish.getTotal();
+        }
+        return total;
+    }
+
+    public static void test() {
+        String[] input = {"ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"};
+        Order order = new Order(input);
+        System.out.println(order.getTotal());
+        for (Dish dish : order.getOrder()){
+            System.out.println(dish.getTotal());
+            System.out.println(dish.getItem().getName());
+        }
+    }
+}
